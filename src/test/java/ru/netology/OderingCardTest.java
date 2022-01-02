c04b5aeb40b3c97311bddb9cebfcfd5edcada51e
chebykina <chebykina.xenia@gmail.com>
2022-01-02 22:29:01 +0300

    Добавление 2 задания

--- src/test/java/ru/netology/OderingCardTest.java
+++ src/test/java/ru/netology/OderingCardTest.java
@@ -36,6 +36,7 @@ public class OderingCardTest {
         driver = null;
     }
 
+    //Заполняем все поля. Заявка оформляется успешно
     @Test
     public void shouldSendForm() {
         driver.get("http://localhost:9999/");
@@ -46,9 +47,84 @@ public class OderingCardTest {
         driver.findElement(By.cssSelector("button")).click();
 
         String actualText = driver.findElement(By.cssSelector("[data-test-id='order-success']")).getText().trim();
-
         String expectedText = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
 
         assertEquals(expectedText, actualText, "Текст сообщения не совпадает");
     }
+
+    //Заполняем тоолько поле фамилия и имя
+    @Test
+    public void enterNameTest() {
+        driver.get("http://localhost:9999/");
+
+        driver.findElement(By.cssSelector("[type='text']")).sendKeys("Иванов Иван");
+        driver.findElement(By.cssSelector("button")).click();
+        driver.findElement(By.cssSelector(".input_invalid")).findElement(By.cssSelector(".input__sub"));
+
+        String actualText = (driver.findElement(By.cssSelector(".input_invalid")).findElement(By.cssSelector(".input__sub"))).getText().trim();
+        String expectedText = "Поле обязательно для заполнения";
+
+        assertEquals(expectedText, actualText, "Текст сообщения не совпадает");
+    }
+
+    //Отправляем пустую форму
+    @Test
+    public void emptyFormTest() {
+        driver.get("http://localhost:9999/");
+
+        driver.findElement(By.cssSelector("button")).click();
+
+        String actualText = driver.findElement(By.cssSelector(".input__sub")).getText().trim();
+        String expectedText = "Поле обязательно для заполнения";
+
+        assertEquals(expectedText, actualText, "Текст сообщения не совпадает");
+    }
+
+    //вводим неверное имя - на латынеце
+    @Test
+    public void enterNameLatTest() {
+        driver.get("http://localhost:9999/");
+
+        driver.findElement(By.cssSelector("[type='text']")).sendKeys("Xenia");
+        driver.findElement(By.cssSelector("[type='tel']")).sendKeys("+21236554523");
+        driver.findElement(By.cssSelector(".checkbox__box")).click();
+        driver.findElement(By.cssSelector("button")).click();
+
+        String actualText = (driver.findElement(By.cssSelector("[data-test-id='name']")).findElement(By.cssSelector(".input__sub"))).getText().trim();
+        String expectedText = "Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.";
+
+        assertEquals(expectedText, actualText, "Текст сообщения не совпадает");
+    }
+
+    //вводим телефон, где меньше 11 цифр
+    @Test
+    public void enterTelNot11Test() {
+        driver.get("http://localhost:9999/");
+
+        driver.findElement(By.cssSelector("[type='text']")).sendKeys("Ксения Хомякова");
+        driver.findElement(By.cssSelector("[type='tel']")).sendKeys("+212365");
+        driver.findElement(By.cssSelector(".checkbox__box")).click();
+        driver.findElement(By.cssSelector("button")).click();
+
+        String actualText = (driver.findElement(By.cssSelector("[data-test-id='phone']")).findElement(By.cssSelector(".input__sub"))).getText().trim();
+        String expectedText = "Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.";
+
+        assertEquals(expectedText, actualText, "Текст сообщения не совпадает");
+    }
+
+    //вводим всё, кроме галочки согласия
+    @Test
+    public void enterNotCheckboxBoxTest() {
+        driver.get("http://localhost:9999/");
+
+        driver.findElement(By.cssSelector("[type='text']")).sendKeys("Ксения Хомякова");
+        driver.findElement(By.cssSelector("[type='tel']")).sendKeys("+89565448789");
+        driver.findElement(By.cssSelector("button")).click();
+
+        boolean actual = driver.findElement(By.cssSelector(".input_invalid")).isDisplayed();
+        boolean expected = true;
+
+        assertEquals(expected, actual, "Не совпадает");
+    }
+
 }
